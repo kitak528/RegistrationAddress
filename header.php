1cc030bc7a9ae319a2eb9dcb8211496571e77004
喜多一智 <kitak@istyle.co.jp>
2021-02-04 15:34:21 +0900

    First Commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>RegistrationAddress</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <!-- Header Start -->
+    <header class="site-header">
+      <div class="wrapper site-header__wrapper">
+        <a href="#" class="brand">Brand</a>
+        <nav class="nav">
+          <button class="nav__toggle" aria-expanded="false" type="button">
+            menu
+          </button>
+          <ul class="nav__wrapper">
+            <li class="nav__item"><a href="https://www.google.com">Home</a></li>
+            <li class="nav__item"><a href="#">ログイン</a></li>
+            <li class="nav__item"><a href="#">登録</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!-- Header End -->
+    </body>
+</html>
---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>RegistrationAddress</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<?php
+include('./header.php');
+?>
+
+<body>
+</body>
+  </html>

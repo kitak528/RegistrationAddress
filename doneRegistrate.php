00a78dcefaa88cbe60ca027c6838c777a7cd9862
喜多一智 <kitak@istyle.co.jp>
2021-02-09 16:37:57 +0900

    登録完了画面作成

---
+++ doneRegistrate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>RegistrationAddress</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<?php
+include('./header.php');
+?>
+
+<body>
+    <div class="content">
+        <h1>会員登録が完了しました。</h1>
+        <p>下のボタンよりログインページに移動してください。</p>
+        <br><br>
+        <a href="login.php"><button class="btn">ログインページに移動する</button></a>
+    </div>
+</body>
+</html>

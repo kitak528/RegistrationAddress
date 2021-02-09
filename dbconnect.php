fb87550ae5408d25b1e80f297ce208ff954f3dbb
喜多一智 <kitak@istyle.co.jp>
2021-02-09 16:44:43 +0900

    DB接続修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=RegistrationAddress;host=localhost;charset=utf8mb4', 'root', '');
+    $db = new PDO('mysql:dbname=RegistrationAddress;host=localhost;charset=utf8mb4', 'root');
 }   catch (PDOException $e) {
     echo "データベース接続エラー　：".$e->getMessage();
 }

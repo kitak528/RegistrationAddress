dbdca9c8331aa1c8a3066dbee8169c03845d3048
喜多一智 <kitak@istyle.co.jp>
2021-02-10 18:02:26 +0900

    ログイン完了画面作成

---
+++ doneLogin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>RegistrationAddress</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <div class="content">
+        <h1>ログインが完了しました。</h1>
+        <br><br>
+        <a href="home.php"><button class="btn">ホームに戻る</button></a>
+    </div>
+</body>
+</html>
